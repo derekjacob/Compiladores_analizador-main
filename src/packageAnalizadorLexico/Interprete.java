c8a4cc6e35d8bf39877b96b141377f6a140ff46e
Angel Aguilar <55730232+AngelAguilah@users.noreply.github.com>
2023-06-26 11:26:17 -0600

    Update Interprete.java
    Version final

--- src/packageAnalizadorLexico/Interprete.java
+++ src/packageAnalizadorLexico/Interprete.java
@@ -8,42 +8,31 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Interprete
-{
+public class Interprete{
     static boolean existenErrores = false;
-
     public static void main(String[] args) throws Exception
     {
-
-        if(args.length > 1)
-        {
+        if(args.length > 1){
             System.out.println("Uso correcto: interprete [script]");
-
             // Convención defininida en el archivo "system.h" de UNIX
             System.exit(64);
-        } else if(args.length == 1)
-        {
+        }else if(args.length == 1){
             ejecutarArchivo(args[0]);
-        } else
-        {
+        }else{
             ejecutarPrompt();
         }
     }
 
-    private static void ejecutarArchivo(String path) throws IOException
-    {
+    private static void ejecutarArchivo(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         ejecutar(new String(bytes, Charset.defaultCharset()));
-
         // Se indica que existe un error
         if(existenErrores) System.exit(65);
     }
 
-    private static void ejecutarPrompt() throws IOException
-    {
+    private static void ejecutarPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
-
         for(;;){
             System.out.print(">>> ");
             String linea = reader.readLine();
@@ -53,16 +42,21 @@ public class Interprete
         }
     }
 
-    private static void ejecutar(String source)
-    {
+    private static void ejecutar(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-
         /*for(Token token : tokens){
             System.out.println(token);
         }*/
         Parser parser = new Parser(tokens);
-        parser.parse();
+        if(parser.parse()){
+            GeneradorPostfija gpf = new GeneradorPostfija(tokens);
+            List<Token> postfija = gpf.convertir();
+
+            GeneradorAST gast = new GeneradorAST(postfija);
+            Arbol programa = gast.generarAST();
+            programa.recorrer();
+        }
     }
 
     /*
@@ -70,16 +64,12 @@ public class Interprete
     para reportar los errores:
     Interprete.error(....);
      */
-    static void error(int linea, String mensaje)
-    {
+    static void error(int linea, String mensaje){
         reportar(linea, "", mensaje);
     }
 
-    private static void reportar(int linea, String donde, String mensaje)
-    {
-        System.err.println(
-                "[linea " + linea + "] Error " + donde + ": " + mensaje
-        );
+    private static void reportar(int linea, String donde, String mensaje){
+        System.err.println("[linea " + linea + "] Error " + donde + ": " + mensaje);
         existenErrores = true;
     }
 }
